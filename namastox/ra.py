a2ef1cd7d204d0ae9cc5f11ea5a2768e8b20cd2f
manuel.pastor <manuel.pastor@upf.edu>
2024-03-07 11:03:26 +0100

    added labels to pending task endpoint

--- namastox/ra.py
+++ namastox/ra.py
@@ -208,6 +208,7 @@ class Ra:
             input_node = self.getNode(node_id)
             itask = input_node.getTask()
             olist.append({'id':node_id, 
+                          'label':itask.getLabel(),
                           'name':itask.getName(),
                           'description':itask.getDescriptionText(),
                           'category':itask.getCategoryText()})
@@ -343,6 +344,7 @@ class Ra:
                     workflow_pkl = os.path.join(self.rapath,'workflow.pkl')
                     if os.path.isfile(workflow_pkl):
                         os.remove(workflow_pkl)
+                        
                     LOG.info (f'workflow name updated to {workflow_custom}')
 
         self.workflow = Workflow(self.raname, self.ra['workflow_name'])
