06f9ab7fa022e975f1a53d68d5141ed9c23161c9
manuel.pastor <manuel.pastor@upf.edu>
2024-04-11 17:24:41 +0200

    adding links to the report

--- namastox/report.py
+++ namastox/report.py
@@ -24,6 +24,9 @@ from namastox.logger import get_logger
 from namastox.ra import Ra
 import os, yaml
 import xlsxwriter
+import docx
+from docx import Document
+from docx.shared import Pt
 
 LOG = get_logger(__name__)
 
@@ -226,9 +229,41 @@ def addGeneralSection (document, ra, item):
         document.add_heading(item_title, level=1)
         document.add_paragraph (ra.general[item])
 
+def add_hyperlink(paragraph, url, text):
+    """
+    A function that places a hyperlink within a paragraph object.
+
+    :param paragraph: The paragraph we are adding the hyperlink to.
+    :param url: A string containing the required url
+    :param text: The text displayed for the url
+    :return: The hyperlink object
+    """
+
+    # This gets access to the document.xml.rels file and gets a new relation id value
+    part = paragraph.part
+    r_id = part.relate_to(url, docx.opc.constants.RELATIONSHIP_TYPE.HYPERLINK, is_external=True)
+
+    # Create the w:hyperlink tag and add needed values
+    hyperlink = docx.oxml.shared.OxmlElement('w:hyperlink')
+    hyperlink.set(docx.oxml.shared.qn('r:id'), r_id, )
+
+    # Create a w:r element
+    new_run = docx.oxml.shared.OxmlElement('w:r')
+
+    # Create a new w:rPr element
+    rPr = docx.oxml.shared.OxmlElement('w:rPr')
+
+    # Join all the xml elements together add add the required text to the w:r element
+    new_run.append(rPr)
+    new_run.text = text
+    hyperlink.append(new_run)
+
+    paragraph._p.append(hyperlink)
+
+    return hyperlink
+
 def report_word (ra):
-    from docx import Document
-    from docx.shared import Pt
+
     # from docx.shared import RGBColor
 
     reportfile = os.path.join (ra.rapath,'report.docx')
@@ -352,28 +387,8 @@ def report_word (ra):
             for ilink in reitem['links']:
                 # if 'include' in ilink and not ilink['include']:
                 #     continue 
-                document.add_paragraph (ilink['label'].replace('_',' ')+' : '+ilink['File'], style='ListBullet')
-
-                # import docx
-                # doc = docx.Document()
-                # p = doc.add_paragraph("Hello World! ")
-
-                # hyperlink = docx.oxml.shared.OxmlElement("w:hyperlink")
-                # hyperlink.set(docx.oxml.shared.qn("w:anchor"), "my_section_id")
-
-                # new_run = docx.oxml.shared.OxmlElement("w:r")
-                # rpr = docx.oxml.shared.OxmlElement("w:rPr")
-
-                # hl = docx.oxml.shared.OxmlElement("w:highlight")
-                # hl.set(docx.oxml.shared.qn("w:val"), "green")
-                # rpr.append(hl)
-
-                # new_run.append(rpr)
-                # new_run.text = "my visible link text"
-
-                # hyperlink.append(new_run)
-                # p._p.append(hyperlink)
-                # doc.save("hyperlink.docx")
+                link_p = document.add_paragraph (ilink['label'].replace('_',' ')+' : ', style='ListBullet')
+                add_hyperlink(link_p, ilink['File'], ilink['File'])
     try:
         document.save(reportfile)
     except:
